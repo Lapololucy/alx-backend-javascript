444750a72b60f0ec1139c1f7fca778b69f264de1
Lapololucy <lapololucy@gmail.com>
2023-07-24 15:43:28 +0000

    created

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,7 @@
-export default function createEmployeesObject(departmentName, ...employees) {
+export default function createEmployeesObject(departmentName, employees) {
   return {
-    [`${departmentName}`]: employees,
+    [departmentName]: [
+      ...employees,
+    ],
   };
 }
