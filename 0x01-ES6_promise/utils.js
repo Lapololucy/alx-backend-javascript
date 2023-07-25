acd8c47476da3754f2fe3e69944730229e99412c
Lapololucy <lapololucy@gmail.com>
2023-07-25 07:10:04 +0000

    requirement packages

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,14 @@
+xport function uploadPhoto() {
+  return Promise.resolve({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+}
+
+
+export function createUser() {
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+}
