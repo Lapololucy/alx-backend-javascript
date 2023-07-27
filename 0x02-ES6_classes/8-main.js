e8ad062ef3b37bef754e769cdcda8b0230acf18a
Lapololucy <lapololucy@gmail.com>
2023-07-27 11:23:17 +0000

    Implement a class named HolbertonClass:

---
+++ 0x02-ES6_classes/8-main.js
@@ -0,0 +1,5 @@
+import HolbertonClass from './8-hbtn_class';
+
+const hc = new HolbertonClass(12, 'Mezzanine');
+console.log(Number(hc)); // Output: 12
+console.log(String(hc)); // Output: Mezzanine
