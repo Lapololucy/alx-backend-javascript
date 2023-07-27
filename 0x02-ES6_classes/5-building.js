3c64f9cd487bae592937912b3d3db964c7ee56f2
Lapololucy <lapololucy@gmail.com>
2023-07-27 10:55:55 +0000

    Implement a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,28 @@
-export default class Building {
+class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  /**
+   * @param {Number} sqft
+   */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
+    }
     this._sqft = sqft;
   }
 
-  // Getter for the sqft attribute
   get sqft() {
     return this._sqft;
   }
-
-  // Abstract method, throws an error if not overridden by subclasses
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
+
+export default Building;
