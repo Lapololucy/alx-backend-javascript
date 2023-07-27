a39d813af8744f92a9ef17cddf5161ee6cab001e
Lapololucy <lapololucy@gmail.com>
2023-07-27 10:26:52 +0000

    implementing a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,50 +1,52 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getter for the name attribute
+  /**
+   * @param {String} name
+   */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
   get name() {
     return this._name;
   }
 
-  // Setter for the name attribute
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new TypeError('Name must be a string');
+  /**
+   * @param {Number} length
+   */
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = length;
   }
 
-  // Getter for the length attribute
   get length() {
     return this._length;
   }
 
-  // Setter for the length attribute
-  set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
+  /**
+   * @param {Array} students
+   */
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
     } else {
-      throw new TypeError('Length must be a number');
+      throw new TypeError('Students must be an Array');
     }
   }
 
-  // Getter for the students attribute
   get students() {
     return this._students;
   }
-
-  // Setter for the students attribute
-  set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
-    } else {
-      throw new TypeError('Students must be an array of strings');
-    }
-  }
 }
 
+export default HolbertonCourse;
