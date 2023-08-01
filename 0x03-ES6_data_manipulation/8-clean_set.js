b9eb37f38260151d25fbc7ec79016be8f001e864
Lapololucy <lapololucy@gmail.com>
2023-08-01 07:29:21 +0000

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return Array.from(set).join('-');
+  }
+
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length))
